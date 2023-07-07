9e4d4db8333041f3d8374f9fef0ee8903c9bcf53
natyyG <natnalecs16@gmail.com>
2023-07-07 06:30:25 -0700

    write to csv file

--- test1.py
+++ test1.py
@@ -1,6 +1,11 @@
 import csv
 
-with open('./sampledata.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for line in csv_reader:
-        print(line)
+# with open('./sampledata.csv', 'r') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     for line in csv_reader:
+#         print(line[3])
+
+with open('./newdata.csv', 'w') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',')
+    csv_writer.writerow(['name', 'email', 'password'])
+    csv_writer.writerow(['naruto', 'naruto@gmail.com', 'minatouzu'])
