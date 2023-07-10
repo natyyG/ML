387116e51091739369af7b3cd66d235cabef6bfe
natyyG <natnalecs16@gmail.com>
2023-07-09 23:47:07 -0700

    algorithm to reverse string

---
+++ reverseString.py
@@ -0,0 +1,6 @@
+x = input("Enter the string you want to reverse: ")
+reverse_list = list(reversed(x))
+reversed_string = ""
+for i in range(len(reverse_list)):
+    reversed_string += reverse_list[i]
+print(reversedih_string)
